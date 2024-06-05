ae416fefbf5324a50f3ef3c195e9a9ad916249e0
m1lmo <omerfarukkoca35@gmail.com>
2024-06-05 13:36:15 +0300

    updated products view initial commit

---
+++ lib/feature/view/tabs/products/widgets/bottom_sheet_child.dart
@@ -0,0 +1,125 @@
+part of '../products_view.dart';
+
+/// Bottom sheet child used for adding new product to the list
+/// in [ProductsView], [BottomSheetChild] is used in [CustomBottomSheet]
+class BottomSheetChild extends StatelessWidget {
+  /// constructor for [BottomSheetChild]
+  const BottomSheetChild({
+    required this.onPressed,
+    required this.textEditingController,
+    required this.pPriceEditingController,
+    required this.sPriceEditingController,
+    super.key,
+  });
+  final VoidCallback? onPressed;
+  final TextEditingController textEditingController;
+  final TextEditingController pPriceEditingController;
+  final TextEditingController sPriceEditingController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.w),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 4.h),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.next,
+                  controller: textEditingController,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                    // FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+                    FilteringTextInputFormatter.deny(RegExp('[0-9]')),
+                  ],
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 2.w,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelStyle: const TextStyle(color: ProjectColors2.primaryContainer),
+                    labelStyle: const TextStyle(
+                      color: ProjectColors2.grey,
+                    ),
+                    labelText: 'Product Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 2.h),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: pPriceEditingController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 2.w,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelStyle: const TextStyle(color: ProjectColors2.primaryContainer),
+                    labelStyle: const TextStyle(
+                      color: ProjectColors2.grey,
+                    ),
+                    labelText: 'Alış Fiyatı', // TODOLOCALIZATION
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 2.h),
+              Expanded(
+                child: TextField(
+                  controller: sPriceEditingController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 2.w,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelStyle: const TextStyle(color: ProjectColors2.primaryContainer),
+                    labelStyle: const TextStyle(
+                      color: ProjectColors2.grey,
+                    ),
+                    labelText: 'Satış Fiyatı', // TODOLOCALIZATION
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 3.h),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(ProjectColors2.primaryContainer),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.sp),
+                ),
+              ),
+            ),
+            onPressed: onPressed,
+            child: Text(
+              'Save',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Colors.black,
+                  ),
+            ), // TODOLOCALIZATION
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/feature/view/tabs/products/widgets/product_data_container.dart
+++ lib/feature/view/tabs/products/widgets/product_data_container.dart
@@ -1,10 +1,108 @@
-import 'package:flutter/material.dart';
+part of '../products_view.dart';
 
+/// Data container for [ProductsView]
 class ProductDataContainer extends StatelessWidget {
-  const ProductDataContainer({super.key});
-
+  /// constructor for [ProductDataContainer]
+  const ProductDataContainer({required this.stock, required this.onPressed, super.key});
+  final StockModel? stock;
+  final VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (stock == null) return const SizedBox.shrink();
+    return Container(
+      height: 11.5.h,
+      decoration: BoxDecoration(
+        color: ProjectColors2.formBackground,
+        borderRadius: BorderRadius.circular(15.sp),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.w),
+                child: Text(
+                  stock!.title ?? '',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+              IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: onPressed,
+                icon: Icon(
+                  Icons.navigate_next_outlined,
+                  size: 30.sp,
+                  color: Colors.black.withOpacity(.40),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Renk',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 10.sp,
+                          ),
+                    ), // TODOLOCALIZATION
+                    Text(
+                      '${stock!.stockDetailModel.length} Renk', // TODOLOCALIZATION
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 10.sp,
+                          ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Satış Fiyatı',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 10.sp,
+                          ),
+                    ), // TODOLOCALIZATION
+                    Text(
+                      '${stock!.sPrice} ${stock!.currency.currencySymbol}', // TODOLOCALIZATION
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 10.sp,
+                          ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Miktar',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 10.sp,
+                          ),
+                    ), // TODOLOCALIZATION
+                    Text(
+                      '${stock!.totalMeter}m', //TODOLOCALIZATION
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 10.sp,
+                          ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
