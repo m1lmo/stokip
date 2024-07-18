2e31da67e080af1c44469d4a5389368c8d563371
m1lmo <omerfarukkoca35@gmail.com>
2024-07-18 18:26:32 +0300

    changed json keys and added explicitToJson parameter to JsonSerializable annotation on [CustomerModel]

--- lib/feature/model/customer_model.dart
+++ lib/feature/model/customer_model.dart
@@ -14,12 +14,13 @@ part 'customer_model.g.dart';
 @HiveType(typeId: HiveTypes.customerModelId)
 
 /// The `CustomerModel` class is a data model class that represents a customer.
+@JsonSerializable(explicitToJson: true)
 final class CustomerModel extends MainModel {
   /// constructor for the CustomerModel class.
   CustomerModel({
     required this.id,
     this.title,
-    this.balance,
+    this.balance = 0,
     this.boughtProducts,
     this.currency = CurrencyEnum.usd,
   });
@@ -29,17 +30,21 @@ final class CustomerModel extends MainModel {
     return _$CustomerModelFromJson(json);
   }
   @HiveField(0)
+  @JsonKey(name: 'customerId')
   final int id;
   @override
   @HiveField(1)
+  @JsonKey(name: 'name')
   final String? title;
   @HiveField(2)
   double? balance;
   @HiveField(3)
+  @JsonKey(includeFromJson: false, includeToJson: false)
   late final List<SalesModel>? boughtProducts;
   @HiveField(4)
   final List<PaymentModel> payments = const [];
   @HiveField(5)
+  @JsonKey(name: 'currencyType')
   final CurrencyEnum currency;
 
   @override
@@ -49,6 +54,7 @@ final class CustomerModel extends MainModel {
   String get key => id.toString();
 
   /// The `toJson` method is a method that converts a `CustomerModel` object into a `Map<String, dynamic>`.
+  @override
   Map<String, dynamic> toJson() {
     return _$CustomerModelToJson(this);
   }
