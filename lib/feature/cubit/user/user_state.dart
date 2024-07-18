41e4e41572cc4d494abfe94c5da8a77a37954d0c
m1lmo <omerfarukkoca35@gmail.com>
2024-07-18 23:05:41 +0300

    fixed issue props objects can be null

--- lib/feature/cubit/user/user_state.dart
+++ lib/feature/cubit/user/user_state.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'user_cubit.dart';
 
-class UserState extends Equatable {
+class UserState with EquatableMixin {
   const UserState({this.currentUser});
   final UserModel? currentUser;
 
   @override
-  List<Object> get props => [currentUser!];
+  List<Object?> get props => [currentUser];
+
+  @override
+  int get hashCode => currentUser.hashCode;
 
   UserState copyWith({
     UserModel? currentUser,
