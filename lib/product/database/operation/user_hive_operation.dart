d08e4b7ece9296a8417f713c75798e32eefa77d9
m1lmo <omerfarukkoca35@gmail.com>
2024-07-18 18:45:45 +0300

    add user_hive_operation

---
+++ lib/product/database/operation/user_hive_operation.dart
@@ -0,0 +1,4 @@
+import 'package:stokip/feature/model/user_model.dart';
+import 'package:stokip/product/database/core/database_opretion.dart';
+
+final class UserHiveOperation extends HiveDataBaseOperation<UserModel> {}
