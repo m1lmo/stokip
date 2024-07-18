7077317b71b614c5e1eb78977e38bca7a8f31ef8
m1lmo <omerfarukkoca35@gmail.com>
2024-07-18 18:44:58 +0300

    added userModelId

--- lib/product/database/core/hive_types.dart
+++ lib/product/database/core/hive_types.dart
@@ -1,11 +1,12 @@
-final class HiveTypes{
+final class HiveTypes {
   const HiveTypes._();
   static const int stockModelId = 0;
   static const int stockDetailModelId = 1;
-  static const int salesModelId =2;
-  static const int importerModelId =3;
-  static const int currencyEnumId =4;
-  static const int purchasesModelId=5;
-  static const int paymentModelId=6;
+  static const int salesModelId = 2;
+  static const int importerModelId = 3;
+  static const int currencyEnumId = 4;
+  static const int purchasesModelId = 5;
+  static const int paymentModelId = 6;
   static const int customerModelId = 7;
+  static const int userModelId = 8;
 }
