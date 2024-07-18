280115c94974770722095be6e7ae99bf6b75699c
m1lmo <omerfarukkoca35@gmail.com>
2024-07-18 18:29:10 +0300

    added postWithResponse method to [IRepository]

--- lib/feature/service/i_repository.dart
+++ lib/feature/service/i_repository.dart
@@ -1,24 +1,37 @@
 import 'dart:io';
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 
+import 'package:dio/dio.dart';
 import 'package:stokip/feature/service/model/service_model.dart';
 import 'package:stokip/feature/service/manager/service_manager.dart';
+import 'package:stokip/product/helper/error_handler.dart';
 
 /// Base class for repository
 /// extend the class with [ServiceModel] to use the repository
 abstract class IRepository<T extends ServiceModel> extends ServiceManager<T> {
   Future<bool> postData(T data) async {
-    final response = await dio.post<T>(path, data: data.toJson());
+    final response = await dio.post<Map<String, dynamic>>(path, data: data.toJson());
+    print(response.data);
     return response.statusCode == HttpStatus.created;
   }
 
-  Future<List<T>?> fetchData() async {
-    final response = await dio.get<Map<String, dynamic>>(path).call();
+  Future<List<T?>?> fetchData() async {
+    final response = await dio.get<List<dynamic>>(path);
     if (response.statusCode != HttpStatus.ok) return null;
     final datas = response.data;
     if (datas is List) {
-      return datas.whereType<Map<String, dynamic>>().map((e) => fromJson(e)).toList();
+      if (datas.isEmpty ?? true) return null;
+      return datas.whereType<Map<String, dynamic>>().map((e) => fromJson(e)).toList(); // Replace whereTypeOrNull with whereType
     }
     return null;
+  }
+
+  Future<Response<Map<String, dynamic>>?> postWithResponse(T data) async {
+    try {
+      final response = await dio.post<Map<String, dynamic>>(path, data: data.toJson());
+      return response;
+    } on DioException catch (e) {
+      print(ErrorHandler.handler(e).failure);
+      return null;
+    }
   }
 }
