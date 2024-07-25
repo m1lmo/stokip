231a3f806d6c7a299b2f8968c9529f9f4c41ebe2
m1lmo <omerfarukkoca35@gmail.com>
2024-07-25 14:50:34 +0300

    chore: Add error handling method and postWithResponse for token

--- lib/feature/service/i_repository.dart
+++ lib/feature/service/i_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:stokip/feature/model/user_model.dart';
 import 'package:stokip/feature/service/model/service_model.dart';
 import 'package:stokip/feature/service/manager/service_manager.dart';
 import 'package:stokip/product/helper/error_handler.dart';
@@ -10,9 +11,18 @@ import 'package:stokip/product/widgets/c_notify.dart';
 /// extend the class with [ServiceModel] to use the repository
 abstract class IRepository<T extends ServiceModel> extends ServiceManager<T> {
   Future<bool> postData(T data) async {
-    final response = await dio.post<Map<String, dynamic>>(path, data: data.toJson());
-    print(response.data);
-    return response.statusCode == HttpStatus.created;
+    try {
+      print(data.toJson());
+      final response = await dio.post<Map<String, dynamic>>(path, data: data.toJson());
+      return response.statusCode == HttpStatus.created;
+    } on DioException catch (e) {
+      final failure = ErrorHandler.handler(e).failure;
+      CNotify(
+        title: 'Hata',
+        message: failure.message,
+      ).show();
+      return false;
+    }
   }
 
   Future<List<T?>?> fetchData() async {
@@ -29,15 +39,11 @@ abstract class IRepository<T extends ServiceModel> extends ServiceManager<T> {
   Future<T?> postWithResponse(T data) async {
     try {
       final response = await dio.post<Map<String, dynamic>>(path, data: data.toJson());
-      final datas = response.data;
+      final responseData = response.data;
       if (response.statusCode != HttpStatus.ok) return null;
-      if (datas is Map<String, dynamic>) {
-        return fromJson(datas);
+      if (responseData is Map<String, dynamic>) {
+        return fromJson(responseData);
       }
-      // if (datas is List) {
-      //   if (datas?.isEmpty ?? true) return null;
-      //   return (datas! as List).whereType<Map<String, dynamic>>().map((e) => fromJson(e)).toList(); // Replace whereTypeOrNull with whereType
-      // }
     } on DioException catch (e) {
       final failure = ErrorHandler.handler(e).failure;
       CNotify(
@@ -47,5 +53,15 @@ abstract class IRepository<T extends ServiceModel> extends ServiceManager<T> {
       return null;
     }
     return null;
+  }
+
+  Future<UserModel> postToken() async {
+    final response = await dio.post<Map<String, dynamic>>(path);
+    final responseData = response.data;
+    if (response.statusCode != HttpStatus.ok) return UserModel();
+    if (responseData is Map<String, dynamic>) {
+      return UserModel.fromJson(responseData);
+    }
+    return UserModel();
   }
 }
